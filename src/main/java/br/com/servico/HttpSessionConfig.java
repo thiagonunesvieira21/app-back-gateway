9549d911e7c889223060a7636d09f276b3772395
Thiago Nunes Vieira <thiagonunesvieira21@gmail.com>
2017-11-27 13:10:16 -0200

    Remoção da classes comuns para o projeto app-back-util

--- src/main/java/br/com/servico/HttpSessionConfig.java
+++ src/main/java/br/com/servico/HttpSessionConfig.java
@@ -2,18 +2,19 @@ package br.com.servico;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.MapSessionRepository;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * Created by thiago on 21/08/17.
  */
 @Configuration
-@EnableSpringHttpSession
+@EnableRedisHttpSession
 public class HttpSessionConfig {
-
-    @Bean
-    public MapSessionRepository sessionRepository() {
-        return new MapSessionRepository();
-    }
+	
+	@Bean
+	JedisConnectionFactory connectionFactory() {
+	    return new JedisConnectionFactory();
+	}
+	
 }
